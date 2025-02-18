f0b028eadbac535c84311f5c921716b086ce149c
SharmilaR03 <sharmilaradhakrishnan58@gmail.com>
2025-02-18 16:11:48 +0530

    messsage

--- python/divisible_by_5.py
+++ python/divisible_by_5.py
@@ -1,5 +1,7 @@
-def check_divisibility(num):
-  # // Expected output is "TRUE" or "FALSE"
-
 num = int(input("Enter a number: "))
-print(check_divisibility(num))
+if(num%5==0 ):
+  print("TRUE")
+else:
+  print("FALSE")
+
+
